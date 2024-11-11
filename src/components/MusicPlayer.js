7cd6ddcd6047a19af20f02e8b98600e270e4d653
Harry Gillen <gillen08@gmail.com>
2024-11-10 22:21:52 -0400

    added back control bar

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -567,13 +567,53 @@ function MusicPlayer({ isFreeflow, onBeginClick, stopAudio, setTimerActive, volu
               onClick={handlePlayPauseClick}
               className="tw-w-12 tw-h-12 tw-flex tw-items-center tw-justify-center tw-bg-gray-600 tw-rounded-full tw-shadow-[0_4px_8px_rgba(0,0,0,0.25)] tw-border-0 tw-outline-none focus:tw-outline-none hover:tw-cursor-pointer tw-transition-all"
             >
-              {/* Play/Pause button content */}
+              {isPlaying ? (
+                // Pause icon
+                <svg 
+                  stroke="currentColor" 
+                  fill="currentColor" 
+                  strokeWidth="0" 
+                  viewBox="0 0 448 512" 
+                  className="tw-text-slate-200 tw-scale-105" 
+                  height="1em" 
+                  width="1em" 
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M144 479H48c-26.5 0-48-21.5-48-48V79c0-26.5 21.5-48 48-48h96c26.5 0 48 21.5 48 48v352c0 26.5-21.5 48-48 48zm304-48V79c0-26.5-21.5-48-48-48h-96c-26.5 0-48 21.5-48 48v352c0 26.5 21.5 48 48 48h96c26.5 0 48-21.5 48-48z" />
+                </svg>
+              ) : (
+                // Play icon
+                <svg 
+                  stroke="currentColor" 
+                  fill="currentColor" 
+                  strokeWidth="0" 
+                  viewBox="0 0 448 512" 
+                  className="tw-text-slate-200 tw-scale-105" 
+                  height="1em" 
+                  width="1em" 
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z" />
+                </svg>
+              )}
             </button>
             <button 
               onClick={handleNextTrackClick}
               className="tw-w-12 tw-h-12 tw-flex tw-flex-row tw-items-center tw-gap-2 tw-justify-center tw-bg-gray-600 tw-rounded-full hover:tw-cursor-pointer tw-transition-all tw-border-0 tw-outline-none focus:tw-outline-none tw-shadow-[0_4px_8px_rgba(0,0,0,0.25)]"
             >
-              {/* Next track button content */}
+              {/* Next Track icon */}
+              <svg 
+                stroke="currentColor" 
+                fill="currentColor" 
+                strokeWidth="0" 
+                viewBox="0 0 448 512" 
+                className="tw-text-slate-200 tw-scale-105" 
+                height="1em" 
+                width="1em" 
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M384 44v424c0 6.6-5.4 12-12 12h-48c-6.6 0-12-5.4-12-12V291.6l-195.5 181C95.9 489.7 64 475.4 64 448V64c0-27.4 31.9-41.7 52.5-24.6L312 219.3V44c0-6.6 5.4-12 12-12h48c6.6 0 12 5.4 12 12z" />
+              </svg>
             </button>
           </div>
         )}

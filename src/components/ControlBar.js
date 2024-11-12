8aca9af833a556e97419d4d675a00c699ff8da44
Harry Gillen <gillen08@gmail.com>
2024-11-12 11:15:55 -0400

    fixed control bar

--- src/components/ControlBar.js
+++ src/components/ControlBar.js
@@ -1,17 +1,54 @@
 import React from 'react';
-import './ControlBar.css';
 
 function ControlBar({ onPlayPauseClick, isPlaying, onNextTrackClick }) {
   return (
-    <div className="control-bar">
-      <button onClick={onPlayPauseClick} className="control-button">
-        <i className={`fa ${isPlaying ? 'fa-pause' : 'fa-play'}`}></i>
+    <div className="tw-absolute tw-bottom-8 tw-left-0 tw-right-0 tw-flex tw-justify-center tw-gap-4 tw-h-[10%]">
+      <button 
+        onClick={onPlayPauseClick}
+        className="tw-w-12 tw-h-12 tw-flex tw-items-center tw-justify-center tw-bg-gray-600 tw-rounded-full tw-shadow-[0_4px_8px_rgba(0,0,0,0.25)] tw-border-0 tw-outline-none focus:tw-outline-none hover:tw-cursor-pointer tw-transition-all"
+      >
+        {isPlaying ? (
+          <svg 
+            stroke="currentColor" 
+            fill="currentColor" 
+            strokeWidth="0" 
+            viewBox="0 0 448 512" 
+            className="tw-text-slate-200 tw-scale-105"
+            height="1em" 
+            width="1em" 
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M144 479H48c-26.5 0-48-21.5-48-48V79c0-26.5 21.5-48 48-48h96c26.5 0 48 21.5 48 48v352c0 26.5-21.5 48-48 48zm304-48V79c0-26.5-21.5-48-48-48h-96c-26.5 0-48 21.5-48 48v352c0 26.5 21.5 48 48 48h96c26.5 0 48-21.5 48-48z" />
+          </svg>
+        ) : (
+          <svg 
+            stroke="currentColor" 
+            fill="currentColor" 
+            strokeWidth="0" 
+            viewBox="0 0 448 512" 
+            className="tw-text-slate-200 tw-scale-105"
+            height="1em" 
+            width="1em" 
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z" />
+          </svg>
+        )}
       </button>
-      <button onClick={onNextTrackClick} className="control-button">
-        <i className="fa fa-step-forward"></i>
+      <button 
+        onClick={onNextTrackClick}
+        className="tw-w-12 tw-h-12 tw-flex tw-flex-row tw-items-center tw-gap-2 tw-justify-center tw-bg-gray-600 tw-rounded-full hover:tw-cursor-pointer tw-transition-all tw-border-0 tw-outline-none focus:tw-outline-none tw-shadow-[0_4px_8px_rgba(0,0,0,0.25)]"
+      >
+        <svg 
+          className="tw-w-6 tw-h-6 tw-fill-white" 
+          viewBox="0 0 24 24"
+        >
+          <path d="M6 18l8.5-6L6 6v12zM16 6v12h2V6h-2z"/>
+        </svg>
       </button>
     </div>
   );
 }
 
 export default ControlBar;
+
